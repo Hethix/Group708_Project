1899d0901cac86677130340e1181663703fb766e
Hethix <An.ryge@gmail.com>
2016-10-19 13:18:28 +0200

    Finished the levels and is ready for testing
    Still needs textures, testing, button and cube

--- Assets/Scripts/ButtonTrigger.cs
+++ Assets/Scripts/ButtonTrigger.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class ButtonTrigger : MonoBehaviour {
 
     public bool isButtonActivated;
-    public byte roomNumber;
+    public byte buttonNumber;
+    private short cubesOnButton;
 
 	// Use this for initialization
 	void Start () {
         isButtonActivated = false;
+        cubesOnButton = 0;
 	}
 	
 	// Update is called once per frame
@@ -16,11 +18,14 @@ public class ButtonTrigger : MonoBehaviour {
 	
 	}
 
-    void onTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Someting on button");
-
-        if (!isButtonActivated && (other.gameObject.name == "CharacterPoint" || other.gameObject.name == "box"));
+        cubesOnButton++;
+        if(buttonNumber == 4 && !isButtonActivated && cubesOnButton >= 5)
+        {
+            isButtonActivated = true;
+        }
+        else if (!isButtonActivated && (other.gameObject.name == "CharacterPoint" || other.gameObject.name == "box"));
         {
             isButtonActivated = true;
         }
@@ -28,7 +33,8 @@ public class ButtonTrigger : MonoBehaviour {
 
     void OnTriggerExit(Collider other)
     {
-        if(roomNumber != 1)
+        cubesOnButton--;
+        if (buttonNumber != 1)
         {
             isButtonActivated = false;
         }
