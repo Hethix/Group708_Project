e82b354a3d244406fc3e1071e8659285110974e1
AAU_SmallLab_PC <An.ryge@gmail.com>
2016-10-26 11:01:59 +0200

    Movement changed to looking direction
    Now moves forward/backward based on the direction looking

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,8 +6,9 @@ public class Movement : MonoBehaviour {
 
     //Headbobbing variables
     public bool Headbobbing;
-    public GameObject Camera;
-    private float[] PosData;
+    public Transform lookingDir;
+    public GameObject camera;
+    private float[] posData;
     private int i = 0;
     private int count = 0;
 
@@ -20,7 +21,8 @@ public class Movement : MonoBehaviour {
     private float movementSpeed = 4f;
     private float sensitivityX = 1.1f;
     private Vector3 playerPos;
-
+    private Vector3 newCameraPosition;
+    
 
     //Velocity variables for movement
     private float initialVelocity = 0.0f;
@@ -38,6 +40,9 @@ public class Movement : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        posData = new float[] {0.1012f,0.1014f,0.1010f,0.1003f,0.0997f,0.0995f,0.0992f,0.0988f,0.0989f,0.0995f,0.1002f,0.1007f,0.1013f,0.1018f,0.1018f,0.1012f,0.1003f,0.0996f,0.0994f,0.0994f,0.0993f,0.0990f
+    ,0.0987f,0.0982f,0.0985f,0.0995f,0.1007f,0.1011f,0.1007f,0.0992f};
+
 
     }
 
@@ -45,9 +50,7 @@ public class Movement : MonoBehaviour {
     void Update () {
         device = SteamVR_Controller.Input((int)controller.index);
 
-        Vector3 newCameraPosition;
-        PosData = new float[] {0.1012f,0.1014f,0.1010f,0.1003f,0.0997f,0.0995f,0.0992f,0.0988f,0.0989f,0.0995f,0.1002f,0.1007f,0.1013f,0.1018f,0.1018f,0.1012f,0.1003f,0.0996f,0.0994f,0.0994f,0.0993f,0.0990f
-    ,0.0987f,0.0982f,0.0985f,0.0995f,0.1007f,0.1011f,0.1007f,0.0992f};
+        
 
 
         if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
@@ -63,18 +66,18 @@ public class Movement : MonoBehaviour {
                 //Headbobbing stuff (Really weird atm)
                 if (Headbobbing)
                 {
-                    for(int i = 0; i < PosData.Length - 1;)
+                    for(int i = 0; i < posData.Length - 1;)
                     {
-                        Camera.transform.position = new Vector3(transform.position.x, transform.position.y + PosData[count] * 10, transform.position.z);
-                        newCameraPosition = Camera.transform.position;
-                        newCameraPosition.y = Camera.transform.position.y;
+                        camera.transform.position = new Vector3(transform.position.x, transform.position.y + posData[count] * 10, transform.position.z);
+                        newCameraPosition = camera.transform.position;
+                        newCameraPosition.y = camera.transform.position.y;
                         if (count < 29){
                             count++;
                         }
                         else{
                             count = 0;
                         }
-                        if(i >= PosData.Length){
+                        if(i >= posData.Length){
                             i = 0;
                         }
                         else {
@@ -99,7 +102,7 @@ public class Movement : MonoBehaviour {
             currentRotationVelocity -= rotationDecelerationRate * Time.deltaTime;
         }
         currentVelocity = Mathf.Clamp(currentVelocity, initialVelocity, finalVelocity);
-        transform.position -= transform.forward * Time.deltaTime * currentVelocity * touchpad.y;
+        transform.position += lookingDir.transform.forward * Time.deltaTime * currentVelocity * touchpad.y;
 
         currentRotationVelocity = Mathf.Clamp(currentRotationVelocity, initialRotationVelocity, finalRotationVelocity);
         transform.Rotate(0,currentRotationVelocity * touchpad.x, 0);
