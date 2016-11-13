3a71707a4990c6b6856e7e9edf1c03802af3d8a9
AAU_SmallLab_PC <An.ryge@gmail.com>
2016-11-13 13:46:08 +0100

    Movement changes
    Now have 2 modes for rotation. Also code it much more clean.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,7 +2,14 @@
 using System.Collections;
 using Valve.VR;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
+    public bool fixedRotation;
+
+    //Field of view variables
+
+
+
 
     //Headbobbing variables
     public bool Headbobbing;
@@ -16,19 +23,19 @@ public class Movement : MonoBehaviour {
     SteamVR_Controller.Device device;
     public SteamVR_TrackedObject controller;
     private Vector2 touchpad;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
 
-
-    private float movementSpeed = 4f;
+    private float movementSpeed = 4.0f;
     private float sensitivityX = 1.1f;
     private Vector3 playerPos;
     private Vector3 newCameraPosition;
-    
+
 
     //Velocity variables for movement
     private float initialVelocity = 0.0f;
     private float finalVelocity = 2.5f;
     private float currentVelocity = 0.0f;
-    private float accelerationRate = 5.0f;
+    private float accelerationRate = 1.5f;
     private float decelerationRate = 10.0f;
 
     //Variables for rotation velocity
@@ -39,7 +46,8 @@ public class Movement : MonoBehaviour {
     private float rotationDecelerationRate = 10.0f;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         posData = new float[] {0.1012f,0.1014f,0.1010f,0.1003f,0.0997f,0.0995f,0.0992f,0.0988f,0.0989f,0.0995f,0.1002f,0.1007f,0.1013f,0.1018f,0.1018f,0.1012f,0.1003f,0.0996f,0.0994f,0.0994f,0.0993f,0.0990f
     ,0.0987f,0.0982f,0.0985f,0.0995f,0.1007f,0.1011f,0.1007f,0.0992f};
 
@@ -47,64 +55,103 @@ public class Movement : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         device = SteamVR_Controller.Input((int)controller.index);
+        int rightIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost);
+        int leftIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost);
 
-        
+        SteamVR_Controller.Device rightDevice = SteamVR_Controller.Input(rightIndex);
+        SteamVR_Controller.Device leftDevice = SteamVR_Controller.Input(leftIndex);
 
 
-        if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
+
+        if (rightDevice.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
         {
-            //Reading the values from touchpad
-            touchpad = device.GetAxis(EVRButtonId.k_EButton_SteamVR_Touchpad);
+            Debug.Log("Pressing right controller");
+            Moving(rightDevice);
+            //  transform.position -= lookingDir.transform.forward * Time.deltaTime * (touchpad.y * 0.5f);
+        } else if (leftDevice.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("Pressing left controller");
+            Moving(leftDevice);
+        }else
+        {
+            currentVelocity -= decelerationRate * Time.deltaTime;
+            currentRotationVelocity -= (rotationDecelerationRate * Time.deltaTime);
+        }
 
-            //Move based on the y input of the touchpad
-            if(touchpad.y > 0.2 || touchpad.y < -0.2)
+        //Doing the movement here after being clamped
+        currentVelocity = Mathf.Clamp(currentVelocity, initialVelocity, finalVelocity);
+        transform.position += lookingDir.transform.forward * Time.deltaTime * currentVelocity * touchpad.y;
+        currentRotationVelocity = Mathf.Clamp(currentRotationVelocity, initialRotationVelocity, finalRotationVelocity);
+        transform.Rotate(0, currentRotationVelocity * touchpad.x, 0);
+    }
+
+
+    private void Moving(SteamVR_Controller.Device device)
+    {
+        //Reading the values from touchpad
+        touchpad = device.GetAxis(EVRButtonId.k_EButton_SteamVR_Touchpad);
+
+        //Move based on the y input of the touchpad
+        if (touchpad.y > 0.2f || touchpad.y < -0.2f)
+        {
+            currentVelocity += (accelerationRate * Time.deltaTime);
+        }
+
+        //Rotation
+        if (fixedRotation)
+        {
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad) && touchpad.x < -0.4f)
             {
-                currentVelocity += (accelerationRate * Time.deltaTime);
-                
-                //Headbobbing stuff (Really weird atm)
-                if (Headbobbing)
-                {
-                    for(int i = 0; i < posData.Length - 1;)
-                    {
-                        camera.transform.position = new Vector3(transform.position.x, transform.position.y + posData[count] * 10, transform.position.z);
-                        newCameraPosition = camera.transform.position;
-                        newCameraPosition.y = camera.transform.position.y;
-                        if (count < 29){
-                            count++;
-                        }
-                        else{
-                            count = 0;
-                        }
-                        if(i >= posData.Length){
-                            i = 0;
-                        }
-                        else {
-                            i++;
-                        }
-                    }
-                }
-                //transform.position -= transform.forward * Time.deltaTime * (touchpad.y * movementSpeed);
+                transform.Rotate(0, -22.5f, 0);
             }
-            
-            
-            
-            //Rotate based on x input of the touchpad
-            if (touchpad.x > 0.3f || touchpad.x < -0.3f)
+            else if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad) && touchpad.x > -0.4f)
             {
-                currentRotationVelocity += (rotationAccelerationRate * Time.deltaTime);
+                transform.Rotate(0, 22.5f, 0);
             }
         }
         else
         {
-            currentVelocity -= decelerationRate * Time.deltaTime;
-            currentRotationVelocity -= rotationDecelerationRate * Time.deltaTime;
+            //Rotate based on x input of the touchpad
+            if (touchpad.x > 0.4f || touchpad.x < -0.4f)
+            {
+                currentRotationVelocity += (rotationAccelerationRate * Time.deltaTime);
+            }
         }
-        currentVelocity = Mathf.Clamp(currentVelocity, initialVelocity, finalVelocity);
-        transform.position += lookingDir.transform.forward * Time.deltaTime * currentVelocity * touchpad.y;
+    }
 
-        currentRotationVelocity = Mathf.Clamp(currentRotationVelocity, initialRotationVelocity, finalRotationVelocity);
-        transform.Rotate(0,currentRotationVelocity * touchpad.x, 0);
+
+    void headbob()
+    {   ////Headbobbing stuff (Really weird atm)
+        //if (Headbobbing)
+        //{
+        //    for (int i = 0; i < posData.Length - 1;)
+        //    {
+        //        camera.transform.position = new Vector3(transform.position.x, transform.position.y + posData[count] * 10, transform.position.z);
+        //        newCameraPosition = camera.transform.position;
+        //        newCameraPosition.y = camera.transform.position.y;
+        //        if (count < 29)
+        //        {
+        //            count++;
+        //        }
+        //        else {
+        //            count = 0;
+        //        }
+        //        if (i >= posData.Length)
+        //        {
+        //            i = 0;
+        //        }
+        //        else {
+        //            i++;
+        //        }
+        //    }
+        //}
+        //transform.position -= transform.forward * Time.deltaTime * (touchpad.y * movementSpeed);
     }
 }
+
+
+
+ 
